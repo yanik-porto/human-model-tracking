3061a8776d6312f176cb0a32f1503fb9c1d95e2b
yanik-porto <yanik.porto@gmail.com>
2023-12-20 08:29:42 +0100

    display computation time

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -1,15 +1,20 @@
 from HPEstimation.HPEstimation import HPEstimation
 from HPEstimation import HPEstimationFactory 
+from settings.utils import AverageMeter
 
+import time
 import cv2
 from HP.utils import draw_keypoints
+
 class Pipeline():
     def __init__(self, config):
         self.config = config
         self.estimator = HPEstimationFactory.Create(config)
 
     def process(self, himages):
+        st = time.time()
         hps = self.estimator.process(himages)
+        self.estim_time.update(time.time() - st)
 
         if self.config.disp:
             for himg in himages:
@@ -17,3 +22,5 @@ class Pipeline():
                 imgOverlay = draw_keypoints(himg.data, hpsImg)
                 cv2.imshow("mywind", imgOverlay)
                 cv2.waitKey(1000)
+        if self.config.verbose:
+            print("time estimation : {est_time.avg:.3f}\t".format(est_time=self.estim_time))
