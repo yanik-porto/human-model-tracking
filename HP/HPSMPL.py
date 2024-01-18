f622ef38cf44d22b7158a0380095119b89c181db
yanik-porto <yanik.porto@gmail.com>
2024-01-18 14:48:40 +0100

    temporary class for saving smpl rendering

---
+++ HP/HPSMPL.py
@@ -0,0 +1,15 @@
+from .HPCoco import HPCoco
+
+import cv2
+
+class HPSMPL(HPCoco):
+    # def __init__(self, image, skeleton, vertices, camera_translation, bbox=None):
+    def __init__(self, image, skeleton, img_rendered, bbox=None):
+        super(HPSMPL, self).__init__(image, skeleton, bbox=bbox)
+
+        self.img_rendered = img_rendered
+
+        cv2.imshow("smpl", img_rendered)
+        cv2.waitKey(1000)
+        # self.vertices = vertices
+        # self.camera_translation = camera_translation
