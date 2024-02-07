7261665de75f418cd78f90d59699a84bde07c33b
yanik-porto <yanik.porto@gmail.com>
2024-02-07 11:39:30 +0100

    unique variable name for all regressor

--- HM/SMPL/smpl.py
+++ HM/SMPL/smpl.py
@@ -10,13 +10,10 @@ class SMPL(_SMPL):
     def __init__(self, *args, **kwargs):
         super(SMPL, self).__init__(*args, **kwargs)
         J_regressor_extra = np.load('HPEstimation/HMR/data/J_regressor_halpe.npy')
-        self.register_buffer('J_regressor_HALPE', torch.tensor(J_regressor_extra, dtype=torch.float32))
-        # self.register_buffer('J_regressor_LSP', torch.tensor(J_regressor_extra, dtype=torch.float32))
+        self.register_buffer('J_regressor_CONVENTION', torch.tensor(J_regressor_extra, dtype=torch.float32))
 
     def forward(self, *args, **kwargs):
         kwargs['get_skin'] = True
         smpl_output = super(SMPL, self).forward(*args, **kwargs)
-        halpe_joints = vertices2joints(self.J_regressor_HALPE, smpl_output.vertices)
-        # lsp_joints = vertices2joints(self.J_regressor_LSP, smpl_output.vertices)
-        # return lsp_joints, smpl_output.vertices
-        return halpe_joints, smpl_output.vertices
+        conv_joints = vertices2joints(self.J_regressor_CONVENTION, smpl_output.vertices)
+        return conv_joints, smpl_output.vertices
