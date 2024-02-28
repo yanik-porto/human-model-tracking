3849115c8cc5cfbde2649fb9f9caaacf441387ef
yanik-porto <yanik.porto@gmail.com>
2024-02-28 14:46:59 +0100

    keep only one detection option

--- HPEstimation/HPEstimation.py
+++ HPEstimation/HPEstimation.py
@@ -1,21 +1,41 @@
 import HM.HM as HM
 from HP.utils import nms
 
+import numpy as np
 from abc import ABC, abstractmethod
-
 class HPEstimation(ABC):
     def __init__(self):
         self.minScoreKpt = 0.1
         self.nmsth = 0.5
+        self.keepOnlyOne = True
 
     def process(self, himages):
         dets = {}
         for himage in himages:
             hps = self.process_image(himage)
             hps = nms(hps, self.nmsth)
+            if self.keepOnlyOne and len(hps) > 1:
+                hps = self.keep_only_one(hps)
             dets[himage.viewpointId] = hps
         return dets
     
     @abstractmethod
     def process_image(self, himage):
         pass
+
+    def keep_only_one(self, hps):
+        maxConf = 0
+        idxMax = -1
+        for i, hp in enumerate(hps):
+            if len(hp.confidences) == 0:
+                idxMax = 0
+                break
+            conf = np.sum(hp.confidences)
+            if conf > maxConf:
+                maxConf = conf
+                idxMax = i
+        if idxMax == -1:
+            print("error, idxmax is still -1")
+            return hps
+        
+        return [hps[idxMax]]
