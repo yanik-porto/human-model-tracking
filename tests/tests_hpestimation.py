1843ed28f11bc04c930d64538ffff736a83209bb
yanik-porto <yanik.porto@gmail.com>
2024-04-04 15:38:57 +0200

    update tests

--- tests/tests_hpestimation.py
+++ tests/tests_hpestimation.py
@@ -3,6 +3,7 @@ import os
 sys.path.insert(0, os.getcwd())
 import unittest
 import cv2
+from pathlib import Path
 
 from HPEstimation.HPEstimationHRNET import HPEstimationHRNET
 from HPEstimation.HPEstimationAlphaPose import HPEstimationAlphaPose
@@ -15,18 +16,18 @@ from SensorData.HImage import HImage
 def run_estimator(estimator, imagePath="tests/000000000785.jpg", save_keypoints=False, save_bbox=False):
     img = cv2.imread(imagePath)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    himg = HImage(img, os.path.basename(imagePath))
+    himg = HImage(img, Path(imagePath))
     dets = estimator.process([himg])
     print(len(dets), " skeleton detected")
 
     if save_keypoints:
-        imgOverlay = draw_keypoints(img, dets)
+        imgOverlay = draw_keypoints(img, dets[himg.viewpointId])
         imgOutPath = imagePath.replace(".jpg", "_kpts_" + estimator.__class__.__name__ + ".jpg")
         imgOverlay = cv2.cvtColor(imgOverlay, cv2.COLOR_RGB2BGR)
         cv2.imwrite(imgOutPath, imgOverlay)
 
     if save_bbox:
-        imgOverlay = draw_bbox(img, dets)
+        imgOverlay = draw_bbox(img, dets[himg.viewpointId])
         imgOutPath = imagePath.replace(".jpg", "_bboxes_" + estimator.__class__.__name__ + ".jpg")
         imgOverlay = cv2.cvtColor(imgOverlay, cv2.COLOR_RGB2BGR)
         cv2.imwrite(imgOutPath, imgOverlay)
