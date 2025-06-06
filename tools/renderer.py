99b269f27a08bde292c3d0aabb24387135fda9d3
yanik-porto <yanik.porto@gmail.com>
2025-06-06 17:00:31 +0200

    rendering inherit from scene3d

--- tools/renderer.py
+++ tools/renderer.py
@@ -6,18 +6,23 @@ import numpy as np
 import pyrender
 import trimesh
 import cv2
+from copy import deepcopy
 
-class Renderer:
+from .viz_utils import create_cube_mesh
+from .matrix import *
+from .scene3d import Scene3D
+
+class Renderer(Scene3D):
     """
     Renderer used for visualizing the SMPL model
     Code adapted from https://github.com/vchoutas/smplify-x
     """
-    def __init__(self, focal_length=5000, img_res=224, faces=None):
+    def __init__(self, focal_length=5000, img_res=224, faces=None,  img_res_height=None):
+        super().__init__(focal_length=focal_length, img_res=img_res, img_res_height=img_res_height)
+        height = img_res if img_res_height is None else img_res_height
         self.renderer = pyrender.OffscreenRenderer(viewport_width=img_res,
-                                       viewport_height=img_res,
+                                       viewport_height=height,
                                        point_size=1.0)
-        self.focal_length = focal_length
-        self.camera_center = [img_res // 2, img_res // 2]
         self.faces = faces
 
     def visualize_tb(self, vertices, camera_translation, images):
@@ -39,7 +44,8 @@ class Renderer:
             alphaMode='OPAQUE',
             baseColorFactor=(0.8, 0.3, 0.3, 1.0))
 
-        camera_translation[0] *= -1.
+        cam_trans = deepcopy(camera_translation)
+        cam_trans[0] *= -1.
 
         mesh = trimesh.Trimesh(vertices, self.faces)
         rot = trimesh.transformations.rotation_matrix(
@@ -51,59 +57,91 @@ class Renderer:
         scene.add(mesh, 'mesh')
 
         camera_pose = np.eye(4)
-        camera_pose[:3, 3] = camera_translation
+        camera_pose[:3, 3] = cam_trans
         camera = pyrender.IntrinsicsCamera(fx=self.focal_length, fy=self.focal_length,
                                            cx=self.camera_center[0], cy=self.camera_center[1])
+
         scene.add(camera, pose=camera_pose)
 
 
-        light = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=1)
-        light_pose = np.eye(4)
+        # light = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=1)
+        # light_pose = np.eye(4)
 
-        light_pose[:3, 3] = np.array([0, -1, 1])
-        scene.add(light, pose=light_pose)
+        # light_pose[:3, 3] = np.array([0, -1, 1])
+        # scene.add(light, pose=light_pose)
 
-        light_pose[:3, 3] = np.array([0, 1, 1])
-        scene.add(light, pose=light_pose)
+        # light_pose[:3, 3] = np.array([0, 1, 1])
+        # scene.add(light, pose=light_pose)
 
-        light_pose[:3, 3] = np.array([1, 1, 2])
-        scene.add(light, pose=light_pose)
+        # light_pose[:3, 3] = np.array([1, 1, 2])
+        # scene.add(light, pose=light_pose)
+
+        # Create camera node and add it to pyRender scene
+        camera_node = pyrender.Node(camera=camera, matrix=camera_pose)
+        scene.add_node(camera_node)
+        self.add_lighting(scene, camera_node)
+
+        if joints is not None:
+            self.add_joints(joints, scene)
 
         color, rend_depth = self.renderer.render(scene, flags=pyrender.RenderFlags.RGBA)
         color = color.astype(np.float32) / 255.0
         valid_mask = (rend_depth > 0)[:,:,None]
-        output_img = (color[:, :, :3] * valid_mask +
-                  (1 - valid_mask) * image)
+        output_img = (color[:, :, :3] * valid_mask + (1 - valid_mask) * image)
         
-        if joints is not None:
-            output_img = self.render_joints(joints, camera_translation, output_img)
+        # output_img *= 255
+        # output_img = output_img.astype(np.uint8)
+
+        output_img = cv2.normalize(output_img, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8U)
+        
+        # if joints is not None:
+        #     output_img = self.render_joints(joints, camera_translation, output_img)
 
         return output_img
+    
+    def add_joints(self, joints, scene):
+        material = pyrender.MetallicRoughnessMaterial(
+            metallicFactor=0.2,
+            alphaMode='OPAQUE',
+            baseColorFactor=(0.2, 0.3, 0.8, 1.0))
 
-    def project_joints(self, joints, camera_translation):
-        camera_pose = np.eye(4)
-        camera_pose[:3, 3] = camera_translation
+        rot = rotation_3d_x(np.radians(180))
+        jHomo = np.concatenate((joints, np.ones((joints.shape[0], 1))), axis=1).transpose()
+        jRotated = rot @ jHomo
+        joints = jRotated.transpose()[:, :3]
 
-        K = np.eye(3)
-        K[0][0] = self.focal_length
-        K[1][1] = self.focal_length
-        K[0][2] = self.camera_center[0]
-        K[1][2] = self.camera_center[1]
+        for i, joint in enumerate(joints):
+            mesh = create_cube_mesh(joint)
+            mesh = pyrender.Mesh.from_trimesh(mesh, material)
 
-        Khomo = K @ np.concatenate((np.eye(3),np.zeros((3,1))), axis=1)
-        P = Khomo @ camera_pose
-        joints2d = P @ np.concatenate((joints, np.ones((joints.shape[0], 1))), axis=1).transpose()
-        joints2d[:, :] /= joints2d[2, :]
-        return joints2d[:2, :].transpose()
+            scene.add(mesh, 'joint_' + str(i))
 
-    def render_joints(self, joints, camera_translation, image):
-        joints2d = self.project_joints(joints, camera_translation)
-        imageOverlay = image.copy()
-        for ikpt in range(joints2d.shape[0]):
-            kptInt = (int(joints2d[ikpt][0]), int(joints2d[ikpt][1]))
-            cv2.circle(imageOverlay, kptInt, radius=2, color=(0,0,255), thickness=3)
+    def add_lighting(self, scene, cam_node, color=np.ones(3), intensity=1.0):
+        light_poses = get_light_poses()
+        light_poses.append(np.eye(4))
+        cam_pose = scene.get_pose(cam_node)
+        for i, pose in enumerate(light_poses):
+            matrix = cam_pose @ pose
+            node = pyrender.Node(
+                name=f"light-{i:02d}",
+                light=pyrender.DirectionalLight(color=color, intensity=intensity),
+                matrix=matrix,
+            )
+            if scene.has_node(node):
+                continue
+            scene.add_node(node)
 
-        return imageOverlay
+def get_light_poses(n_lights=5, elevation=np.pi / 3, dist=12):
+    # get lights in a circle around origin at elevation
+    thetas = elevation * np.ones(n_lights)
+    phis = 2 * np.pi * np.arange(n_lights) / n_lights
+    poses = []
+    trans = make_translation(torch.tensor([0, 0, dist]))
+    for phi, theta in zip(phis, thetas):
+        rot = make_rotation(rx=-theta, ry=phi, order="xyz")
+        poses.append((rot @ trans).numpy())
+    return poses
+
 
 def Get3Dfrom2D(List2D, K, R, t, d=1.75):
     # List2D : n x 2 array of pixel locations in an image
