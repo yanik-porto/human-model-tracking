76aa5cdfed31c8b2b1870f6d882a0c1a3b1a1a7b
yanik-porto <yanik.porto@gmail.com>
2025-06-06 17:22:20 +0200

    update config

--- settings/config.py
+++ settings/config.py
@@ -1,12 +1,15 @@
 class Config():
     def __init__(self):
-        self.estimator = "hrnet"
-        self.target_fps = 25
+        # self.estimator = "hrnet"
+        # self.estimator = "yolo_pose"
+        self.estimator = "hmr"
+        self.estimator_shape = None
+        self.target_fps = 10
         self.sampling_by_sec = 2
         self.disp = True
-        self.estimator_shape = None
-        self.verbose = True
-        self.save_projections = True
+        self.verbose = False
+        self.estimate_action = False
+        # self.save_projections = True
 
 def load_config():
     config = Config()
