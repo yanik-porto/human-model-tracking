5f0a96010efe0d5efc046e4a33348180c8513e41
yanik-porto <yanik.porto@gmail.com>
2025-10-13 12:00:09 +0200

    arg for estimating pose by file instead of by folder

--- Pipeline/runner_folder.py
+++ Pipeline/runner_folder.py
@@ -1,5 +1,6 @@
 import time
 import os
+import numpy as np
 from pathlib import Path
 
 from .pipeline import Pipeline
@@ -9,26 +10,7 @@ from settings.utils import AverageMeter
 import imageio.v3 as iio
 from tqdm import tqdm
 
-
-def run_pipeline(folder_path, config, use_tqdm=True, plugin="FFMPEG"):
-    print(f"Estimage 2d pose for folder {os.path.dirname(folder_path)}")
-    pipeline = Pipeline(config)
-
-    # Collect videos info
-    metaByVpath = {}
-    maxIdxAll = 0
-    for root, _, files in os.walk(folder_path):
-        for f in files:
-            if not f.endswith(('.mkv', '.mp4', '.avi')):
-                continue
-
-            fpath = os.path.join(root, f)
-            meta = get_video_info(fpath)
-            metaByVpath[fpath] = meta
-
-            if meta['maxIdx'] > maxIdxAll:
-                maxIdxAll = meta['maxIdx']
-
+def run_pipeline(pipeline, metaByVpath, maxIdxAll, config, use_tqdm=True, plugin="FFMPEG"):
     # propagate camera information
     pipeline.set_viewpoints(metaByVpath)
 
@@ -69,4 +51,56 @@ def run_pipeline(folder_path, config, use_tqdm=True, plugin="FFMPEG"):
     print("time loading : {est_time.avg:.3f}\t".format(est_time=loading_time))
     pipeline.print_stats()
 
+    
+def run_pipeline_on_folder(folder_path, config, use_tqdm=True, plugin="FFMPEG"):
+    print(f"Estimage 2d pose for folder {os.path.dirname(folder_path)}")
+    pipeline = Pipeline(config)
+
+    # Collect videos info
+    metaByVpath = {}
+    maxIdxAll = 0
+    for root, _, files in os.walk(folder_path):
+        for f in files:
+            if not f.endswith(('.mkv', '.mp4', '.avi')):
+                continue
+
+            fpath = os.path.join(root, f)
+            meta = get_video_info(fpath)
+            metaByVpath[fpath] = meta
+
+            if meta['maxIdx'] > maxIdxAll:
+                maxIdxAll = meta['maxIdx']
+
+            # check if video contains saved bbox
+            bbox_path = os.path.splitext(fpath)[0] + "_bbox.npy"
+            if os.path.exists(bbox_path):
+                bboxs = np.load(bbox_path).astype(int)
+                metaByVpath[fpath]['bboxs'] = bboxs
+                print(f"  Found video {fpath}, {meta['maxIdx']} frames, bboxs: {bboxs.shape[0]}")
+
+    run_pipeline(pipeline, metaByVpath, maxIdxAll, config, use_tqdm=use_tqdm, plugin=plugin)
+    return pipeline
+
+def run_pipeline_on_file(file_path, config, use_tqdm=True, plugin="FFMPEG"):
+    print(f"Estimage 2d pose for file {file_path}")
+    pipeline = Pipeline(config)
+
+    # Collect videos info
+    metaByVpath = {}
+    maxIdxAll = 0
+
+    meta = get_video_info(file_path)
+    metaByVpath[file_path] = meta
+
+    if meta['maxIdx'] > maxIdxAll:
+        maxIdxAll = meta['maxIdx']
+
+    # check if video contains saved bbox
+    bbox_path = os.path.splitext(file_path)[0] + "_bbox.npy"
+    if os.path.exists(bbox_path):
+        bboxs = np.load(bbox_path).astype(int)
+        metaByVpath[file_path]['bboxs'] = bboxs
+        print(f"  Found video {file_path}, {meta['maxIdx']} frames, bboxs: {bboxs.shape[0]}")
+
+    run_pipeline(pipeline, metaByVpath, maxIdxAll, config, use_tqdm=use_tqdm, plugin=plugin)
     return pipeline
--- estimate_pose_2d.py
+++ estimate_pose_2d.py
@@ -4,17 +4,21 @@ import glob
 import concurrent.futures
 
 from settings.config import load_config
-from Pipeline.runner_folder import run_pipeline
+from Pipeline.runner_folder import run_pipeline_on_folder, run_pipeline_on_file
 
 def parse_args():
     parser = argparse.ArgumentParser("Read multiple set of videos and save pose estimation")
     parser.add_argument("dataset_folder_path", type=str, help="Path to the folder where the dataset is stored")
     parser.add_argument("--overwrite", action="store_true", default=False, help="If set, overwrite existing estimation files")
     parser.add_argument("--num_workers", type=int, default=1, help="Number of workers to run in parallel")
+    parser.add_argument("--by_file", action="store_true", default=False, help="If set, iterate by video files, instead of by folder")
     return parser.parse_args()
 
-def run_pipeline_and_save_projections(folder_path, config, use_tqdm=True):
-    pipeline = run_pipeline(folder_path, config, use_tqdm=use_tqdm, plugin="pyav")
+def run_pipeline_and_save_projections(folder_path, config, use_tqdm=True, by_file=False):
+    if by_file:
+        pipeline = run_pipeline_on_file(folder_path, config, use_tqdm=use_tqdm)#, plugin="pyav")
+    else:
+        pipeline = run_pipeline_on_folder(folder_path, config, use_tqdm=use_tqdm)
     pipeline.hp_manager.save_projections()
 
 if __name__ == "__main__":
@@ -24,20 +28,37 @@ if __name__ == "__main__":
 
     paths = []
     for root, folders, files in os.walk(args.dataset_folder_path):
-        for folder in folders:
-            folder_path = os.path.join(root, folder)
+        if args.by_file:
+            for f in files:
+                if not f.endswith(('.mkv', '.mp4', '.avi')):
+                    continue
 
-            if not args.overwrite:
-                npz_files = glob.glob(os.path.join(folder_path, "*[0-9].npz"))
+                file_path = os.path.join(root, f)
+                npz_files = glob.glob(os.path.splitext(file_path)[0] + "*[0-9].npz")
                 if len(npz_files) > 0:
-                    print(f"Skip estimation for {folder}, files exist yet")
-                    continue 
+                    if not args.overwrite:
+                        print(f"Skip estimation for {f}, files exist yet")
+                        continue
+                    else:
+                        print(f"Delete existing estimation files for {f}")
+                        for npz_f in npz_files:
+                            os.remove(npz_f)
+                paths.append(file_path)
 
-            paths.append(folder_path)
+        else:                
+            for folder in folders:
+                folder_path = os.path.join(root, folder)
+
+                if not args.overwrite:
+                    npz_files = glob.glob(os.path.join(folder_path, "*[0-9].npz"))
+                    if len(npz_files) > 0:
+                        print(f"Skip estimation for {folder}, files exist yet")
+                        continue 
+                paths.append(folder_path)
 
     if args.num_workers == 1:
         for path in paths:
-            run_pipeline_and_save_projections(path, config, use_tqdm=True)
+            run_pipeline_and_save_projections(path, config, use_tqdm=True, by_file=args.by_file)
     else:
         with concurrent.futures.ThreadPoolExecutor(max_workers=args.num_workers) as executor:
             futur_to_path = {executor.submit(run_pipeline_and_save_projections, path, config, False): path for path in paths}
